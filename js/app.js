84784e3315a615af9bbbad1419bca07cc7479672
Janusred <horaciok300@hotmail.com>
2022-10-04 06:02:01 -0500

    Manejo de fetchEvent

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@
 }*/
 
 if (navigator.serviceWorker){
-
+navigator.serviceWorker.register('/sw.js');
 }
+
+fetch('https://reques.in/api/users')
+.then(resp=>resp.text())
+.then(console.log)
--- sw.js
+++ sw.js
@@ -5,10 +5,27 @@ self.addEventListener('fetch', event =>{
     //console.log(event);
     //console.log('SW: hola Janus ');
     if(event.request.url.includes('.jpg')){
-       // event.respondWith( null);
-      console.log(event.request.url);
-      let fotoReq = fetch (event.request);
-      event.respondWith(fotoReq);
-    
+        const instalacion = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('SW:Fin de la instalacion');
+                self.skipWaiting();
+                resolve();
+            }, 10);
+        });
+        event.waitUntil(instalacion);
+    }
+});
+
+self.addEventListener('activate', event => {
+    //Se borra cache
+    console.log('SW2: Activo y listo para controlar la app');
+});
+self.addEventListener('fetch', event => {
+    //Aplica cache
+    console.log('SW', event.request.url);
+
+    if (event.request.url.includes('https://reqres.in')) {
+        const resp = new Response(`{  ok:false, mensaje: 'Janus' }`)
+        event.respondWith(resp);
     }
 });
