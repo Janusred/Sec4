88bbd4ca3ff6d4389b5f3ec78ba166849e88dde8
Janusred <horaciok300@hotmail.com>
2022-10-04 00:05:58 -0500

    Formas validas eventfetch

--- sw.js
+++ sw.js
@@ -4,10 +4,11 @@ self.addEventListener('fetch', event =>{
     
     //console.log(event);
     //console.log('SW: hola Janus ');
-    if(event.request.url.include(style.css)){
-        event.respondWith( null);
-
-    }else{
-        event.respondWith( fetch(event.request));
+    if(event.request.url.includes('.jpg')){
+       // event.respondWith( null);
+      console.log(event.request.url);
+      let fotoReq = fetch (event.request);
+      event.respondWith(fotoReq);
+    
     }
 });
